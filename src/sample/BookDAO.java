3b78127053bc8493e2bbbaaa148600a41416e7e6
Rijad Burović <rburovic1@etf.unsa.ba>
2023-03-10 13:18:39 +0100

    Create, read, update, delete (CRUD) implemented

--- src/sample/BookDAO.java
+++ src/sample/BookDAO.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class BookDAO {
     private static BookDAO instance = null;
     private Connection conn;
-    private PreparedStatement ps;
+    private PreparedStatement searchQuery, insertQuery, updateQuery, deleteQuery, maxIdQuery;
 
     private BookDAO() throws ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
@@ -14,7 +14,11 @@ public class BookDAO {
         try {
             conn = DriverManager.getConnection(url, "root", "password");
 
-            ps = conn.prepareStatement("SELECT * FROM book WHERE title=? OR author=?");
+            searchQuery = conn.prepareStatement("SELECT * FROM book WHERE title=? OR author=?");
+            insertQuery = conn.prepareStatement("INSERT INTO book VALUES (?, ?, ?, ?)");
+            maxIdQuery = conn.prepareStatement("SELECT max(id) FROM book");
+            updateQuery = conn.prepareStatement("UPDATE book SET title=?, author=?, isbn=? WHERE id=?");
+            deleteQuery = conn.prepareStatement("DELETE FROM book WHERE id=?");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -28,9 +32,9 @@ public class BookDAO {
     public ArrayList<Book> searchBook(String search) {
         ArrayList<Book> books = new ArrayList<>();
         try {
-            ps.setString(1, search);
-            ps.setString(2, search);
-            ResultSet rs = ps.executeQuery();
+            searchQuery.setString(1, search);
+            searchQuery.setString(2, search);
+            ResultSet rs = searchQuery.executeQuery();
             while (rs.next()) {
                 books.add(new Book(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4)));
             }
@@ -38,5 +42,47 @@ public class BookDAO {
             throwables.printStackTrace();
         }
         return books;
+    }
+
+    public void insertBook(Book b) {
+        try {
+            ResultSet maxId = maxIdQuery.executeQuery();
+            if (maxId.next()) {
+                b.setId(maxId.getInt(1) + 1);
+            }
+            else {
+                b.setId(1);
+            }
+            insertQuery.setInt(1, b.getId());
+            insertQuery.setString(2, b.getTitle());
+            insertQuery.setString(3, b.getAuthor());
+            insertQuery.setString(4, b.getIsbn());
+            insertQuery.execute();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void updateBook(Book b) {
+        try {
+            updateQuery.setInt(4, b.getId());
+            updateQuery.setString(1, b.getTitle());
+            updateQuery.setString(2, b.getAuthor());
+            updateQuery.setString(3, b.getIsbn());
+            updateQuery.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+    public void deleteBook(Book b) {
+        try {
+            deleteQuery.setInt(1, b.getId());
+            deleteQuery.execute();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,76 @@
 package sample;
 
-import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner entry = new Scanner(System.in);
+    private static BookDAO dao;
+
 
     public static void main(String[] args) throws ClassNotFoundException {
-        BookDAO dao = BookDAO.getInstance();
+        dao = BookDAO.getInstance();
 
+        int option = 0;
+        do {
+            System.out.println("Enter the option:\n1 - search\n2 - insert\n3 - update\n4 - delete\n0 - end of program");
+            option = entry.nextInt();
+            if (entry.hasNextLine()) entry.nextLine();
+            switch (option) {
+                case 1:
+                    search();
+                    break;
+                case 2:
+                    insert();
+                    break;
+                case 3:
+                    update();
+                    break;
+                case 4:
+                    delete();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("You haven't chose the right option");
+            }
+
+        } while(option != 0);
+    }
+
+    private static void delete() {
+        System.out.println("Enter id of the book you want to change: ");
+        int id = entry.nextInt();
+        dao.deleteBook(new Book(id, "", "", ""));
+    }
+
+    private static void update() {
+        System.out.println("Enter id of the book you want to change: ");
+        int id = entry.nextInt();
+        if (entry.hasNextLine()) entry.nextLine();
+        System.out.println("Enter the title of the book: ");
+        String title = entry.nextLine();
+        System.out.println("Enter the author of the book: ");
+        String author = entry.nextLine();
+        System.out.println("Enter the isbn of the book: ");
+        String isbn = entry.nextLine();
+        dao.updateBook(new Book(id, title, author, isbn));
+    }
+
+    private static void insert() {
+        System.out.println("Enter the title of the book: ");
+        String title = entry.nextLine();
+        System.out.println("Enter the author of the book: ");
+        String author = entry.nextLine();
+        System.out.println("Enter the isbn of the book: ");
+        String isbn = entry.nextLine();
+        dao.insertBook(new Book(0, title, author, isbn));
+    }
+
+    private static void search() {
         System.out.println("Enter the title or author of the book: ");
-        Scanner entry = new Scanner(System.in);
         String search = entry.nextLine();
         for (Book b : dao.searchBook(search)) {
             System.out.println("ID: "+b.getId()+", Title: "+b.getTitle()+", Author: "+b.getAuthor());
         }
-
     }
 }
